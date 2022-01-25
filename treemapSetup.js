c6868a74da60d9928b2b26e3104451fc602a0101
larissaw <40071882+LariWa@users.noreply.github.com>
2022-01-25 12:32:08 +0100

    adapt to our data

--- treemapSetup.js
+++ treemapSetup.js
@@ -127,14 +127,15 @@ This [treemap](/@d3/treemap) supports zooming: click any cell to zoom in, or the
           console.log("enter");
           // set the dimensions and margins of the graph
           const margin = { top: 20, right: 30, bottom: 40, left: 90 },
-            width = 460 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+            width = 300 - margin.left - margin.right,
+            height = 200 - margin.top - margin.bottom;
 
           // append the svg object to the body of the page
           const svg = d3
             .select("#my_dataviz")
-            .append("svg")
+            .append("div")
             .attr("id", "barchart")
+            .append("svg")
 
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
@@ -144,39 +145,35 @@ This [treemap](/@d3/treemap) supports zooming: click any cell to zoom in, or the
             .style("position", "absolute")
             .style("top", getOffset(event.target).top + 50 + "px")
             .style("left", getOffset(event.target).left + 50 + "px");
-          // Parse the Data
-          d3.csv(
-            "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv"
-          ).then(function (data) {
-            // Add X axis
-            const x = d3.scaleLinear().domain([0, 13000]).range([0, width]);
-            svg
-              .append("g")
-              .attr("transform", `translate(0, ${height})`)
-              .call(d3.axisBottom(x))
-              .selectAll("text")
-              .attr("transform", "translate(-10,0)rotate(-45)")
-              .style("text-anchor", "end");
 
-            // Y axis
-            const y = d3
-              .scaleBand()
-              .range([0, height])
-              .domain(data.map((d) => d.Country))
-              .padding(0.1);
-            svg.append("g").call(d3.axisLeft(y));
+          const x = d3.scaleLinear().domain([0, 10]).range([0, width]);
+          svg
+            .append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "translate(-10,0)rotate(-45)")
+            .style("text-anchor", "end");
+          data = d.data.barData;
+          // Y axis
+          const y = d3
+            .scaleBand()
+            .range([0, height])
+            .domain(data.map((d) => d.name))
+            .padding(0.1);
+          svg.append("g").call(d3.axisLeft(y));
 
-            //Bars
-            svg
-              .selectAll("myRect")
-              .data(data)
-              .join("rect")
-              .attr("x", x(0))
-              .attr("y", (d) => y(d.Country))
-              .attr("width", (d) => x(d.Value))
-              .attr("height", y.bandwidth())
-              .attr("fill", "#69b3a2");
-          });
+          //Bars
+          svg
+            .selectAll("myRect")
+            .data(data)
+            .join("rect")
+            .attr("x", x(0))
+            .attr("y", (d) => y(d.name))
+            .attr("width", (d) => x(d.value))
+            .attr("height", y.bandwidth())
+            .attr("fill", "#69b3a2");
+          d3.select(barchart).append("p").text(d.data.info);
         }
         function endHover() {
           console.log("leave");
